26919021404a0a471f247e364f9b62220608b520
sumeyye <sumeyye98762@gmail.com>
2019-04-21 17:15:07 +0300

    home fragment eklendi.

--- GezginApp/app/src/main/java/com/example/gezginapp/MainActivity.java
+++ GezginApp/app/src/main/java/com/example/gezginapp/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.gezginapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
+
+    NavigationView navigationView;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar
+                , R.string.navigation_drawer_open
+                , R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem
+                                                            menuItem) {
+
+                return false;
+            }
+        });
+
+
+        FragmentTransaction transaction
+                = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, new HomeFragment());
+        transaction.commit();
 
 
     }
---
+++ GezginApp/app/src/main/java/com/example/gezginapp/Place.java
@@ -0,0 +1,13 @@
+package com.example.gezginapp;
+
+public class Place {
+    int placeImage;
+    String placeName;
+    String description;
+
+    public Place(int placeImage,String placeName,String description){
+        this.placeImage=placeImage;
+        this.placeName=placeName;
+        this.description=description;
+    }
+}
